1608584f5969ec1796fdacccfb87799b0d7e57fb
DylanMashini <dylanmashini123@gmail.com>
2022-05-23 15:27:34 -0400

    fix: use ncp instead of cp

--- no-workers/build.sh
+++ no-workers/build.sh
@@ -3,5 +3,5 @@ set -o verbose
 rm -rf dist
 wasm-pack build
 webpack --config ./webpack.config.js --mode production 
-cp -r ./static/ ./dist/
-cp -r ./dist/ ../dist/
+ncp ./static/ ./dist/
+ncp ./dist/ ../dist/
