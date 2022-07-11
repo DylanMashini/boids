edc2f537179e0c3dc9cb2d7465916937150f8d12
DylanMashini <dylanmashini123@gmail.com>
2022-07-11 19:49:46 -0400

    feat: fix error with rust

--- install.sh
+++ install.sh
@@ -13,3 +13,6 @@ if ! command -v wasm-pack &> /dev/null
 then
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 fi
+
+# Add wasm target
+rustup target add wasm32-unknown-unknown
