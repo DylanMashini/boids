28f180841d72bd53a0338cce2933e7cd7289195c
DylanMashini <dylanmashini123@gmail.com>
2023-04-13 11:32:35 -0400

    Refactor: Switch to pnpm AND Feat: Use InstancedMesh

--- versions/rust-workers/ts/boids.ts
+++ versions/rust-workers/ts/boids.ts
@@ -1,60 +1,60 @@
 import * as THREE from "three";
 
 export default class Boids extends THREE.InstancedMesh {
-	boidCount: number;
-	settings: any;
-	dummy: THREE.Object3D;
-	startingPositions: THREE.Object3D[];
-	constructor(scene: THREE.Scene, settings: any) {
-		super(
-			new THREE.ConeBufferGeometry(0.5, 2, 32, 32),
-			1000
-		);
-		this.boidCount = settings.boidCount;
-		this.settings = settings;
-		this.dummy = new THREE.Object3D();
-		this.startingPositions = [];
-		this.randomizeColors();
-		this.randomPositions();
-		scene.add(this);
-	}
-	randomizeColors() {
-		const colorList = [0x8ce68c, 0xabf1bc, 0xaee7f8, 0x87cdf6];
-		for (let i = 0; i < this.boidCount; i++) {
-			const group = Math.floor(Math.random() * colorList.length);
-			this.setColorAt(i, new THREE.Color(colorList[group]));
-		}
-		this.instanceColor.needsUpdate = true;
-	}
-	randomPositions() {
-		for (let i = 0; i < this.boidCount; i++) {
-			const neg = Math.random() > 0.5 ? -1 : 1;
-			let pos = new THREE.Object3D();
-			pos.position.x = Math.random() * (this.settings.boxSize / 2) * neg;
-			pos.position.y = Math.random() * (this.settings.boxSize / 2) * neg;
-			pos.position.z = Math.random() * (this.settings.boxSize / 2) * neg;
-			pos.updateMatrix();
-			this.setMatrixAt(i, pos.matrix);
-			this.startingPositions.push(pos)
-		}
-			this.instanceMatrix.needsUpdate = true;
-	}
-	updateBoids(floatThreads: Float64Array) {
-		for (let i = 0; i < this.count; i++) {
-			this.dummy.position.x = floatThreads[i * 9];
-			this.dummy.position.y = floatThreads[i * 9 + 1];
-			this.dummy.position.z = floatThreads[i * 9 + 2];
-			this.dummy.quaternion.setFromUnitVectors(
-				new THREE.Vector3(0, 1, 0),
-				new THREE.Vector3(
-					floatThreads[i * 9 + 3],
-					floatThreads[i * 9 + 4],
-					floatThreads[i * 9 + 5]
-				).normalize()
-			);
-			this.dummy.updateMatrix();
-			this.setMatrixAt(i, this.dummy.matrix);
-		}
-		this.instanceMatrix.needsUpdate = true;
-	}
+    boidCount: number;
+    settings: any;
+    dummy: THREE.Object3D;
+    startingPositions: THREE.Object3D[];
+    constructor(scene: THREE.Scene, settings: any) {
+        super(
+            new THREE.ConeBufferGeometry(0.5, 2, 32, 32),
+            new THREE.MeshBasicMaterial(),
+            1000
+        );
+        this.boidCount = settings.boidCount;
+        this.settings = settings;
+        this.dummy = new THREE.Object3D();
+        this.startingPositions = [];
+        this.randomizeColors();
+        this.randomPositions();
+        scene.add(this);
+    }
+    randomizeColors() {
+        const colorList = [0x8ce68c, 0xabf1bc, 0xaee7f8, 0x87cdf6];
+        for (let i = 0; i < this.boidCount; i++) {
+            const group = Math.floor(Math.random() * colorList.length);
+            this.setColorAt(i, new THREE.Color(colorList[group]));
+        }
+        this.instanceColor.needsUpdate = true;
+    }
+    randomPositions() {
+        for (let i = 0; i < this.boidCount; i++) {
+            let pos = new THREE.Object3D();
+            pos.position.x = Math.random() * (this.settings.boxSize / 2) * (Math.random() > 0.5 ? -1 : 1);
+            pos.position.y = Math.random() * (this.settings.boxSize / 2) * (Math.random() > 0.5 ? -1 : 1);
+            pos.position.z = Math.random() * (this.settings.boxSize / 2) * (Math.random() > 0.5 ? -1 : 1);
+            pos.updateMatrix();
+            this.setMatrixAt(i, pos.matrix);
+            this.startingPositions.push(pos)
+        }
+        this.instanceMatrix.needsUpdate = true;
+    }
+    updateBoids(floatThreads: Float64Array) {
+        for (let i = 0; i < this.count; i++) {
+            this.dummy.position.x = floatThreads[i * 9];
+            this.dummy.position.y = floatThreads[i * 9 + 1];
+            this.dummy.position.z = floatThreads[i * 9 + 2];
+            this.dummy.quaternion.setFromUnitVectors(
+                new THREE.Vector3(0, 1, 0),
+                new THREE.Vector3(
+                    floatThreads[i * 9 + 3],
+                    floatThreads[i * 9 + 4],
+                    floatThreads[i * 9 + 5]
+                ).normalize()
+            );
+            this.dummy.updateMatrix();
+            this.setMatrixAt(i, this.dummy.matrix);
+        }
+        this.instanceMatrix.needsUpdate = true;
+    }
 }
--- versions/rust-workers/ts/main.ts
+++ versions/rust-workers/ts/main.ts
@@ -7,8 +7,8 @@ import Boids from "./boids";
 
 // Check if webworkers and SharedBufferArray are supported
 if (typeof SharedArrayBuffer == "undefined") {
-	// if they aren't than send to the version that doesn't use SharedArrayBuffer
-	window.location.href = "https://boids.dylanmashini.com/no-workers";
+    // if they aren't than send to the version that doesn't use SharedArrayBuffer
+    window.location.href = "https://boids.dylanmashini.com/no-workers";
 }
 
 let maxThreads = window.navigator.hardwareConcurrency || 4;
@@ -19,21 +19,21 @@ let threadCount: Number;
 
 //set default settings
 let settings = {
-	maxSpeed: 0.5,
-	maxForce: 0.03,
-	neighbohoodSize: 10,
-	boidCount: 1000,
-	boxSize: 200,
-	randomHome: true,
-	colorSeperation: false,
-	sphere: false,
-	highlight: false,
+    maxSpeed: 0.5,
+    maxForce: 0.03,
+    neighbohoodSize: 10,
+    boidCount: 1000,
+    boxSize: 200,
+    randomHome: true,
+    colorSeperation: false,
+    sphere: false,
+    highlight: false,
 };
 
 const instancedMesh = new THREE.InstancedMesh(
-	new THREE.ConeBufferGeometry(0.5, 2, 32, 32),
-	new THREE.MeshBasicMaterial({ color: 0x8ce68c }),
-	1000
+    new THREE.ConeBufferGeometry(0.5, 2, 32, 32),
+    new THREE.MeshBasicMaterial({ color: 0x8ce68c }),
+    1000
 );
 
 //get html form
@@ -51,104 +51,104 @@ form["sphere"].checked = settings.sphere;
 
 //add callback for if the button on the form is pressed
 form.addEventListener("submit", e => {
-	//prevent site from reloading
-	e.preventDefault();
+    //prevent site from reloading
+    e.preventDefault();
 
-	let restartWebWorker = false;
-	if (
-		settings["boidCount"] != Number(form["boidCount"].value) ||
-		settings["maxSpeed"] != Number(form["maxSpeed"].value) ||
-		settings["maxForce"] != Number(form["maxForce"].value) ||
-		settings["neighbohoodSize"] != Number(form["neighbohoodSize"].value) ||
-		settings["colorSeperation"] != form["colorSeperation"].value
-	) {
-		restartWebWorker = true;
-	}
-	//check if boidCount has changed
-	if (settings["boidCount"] != Number(form["boidCount"].value)) {
-		//need to add or remove boids
+    let restartWebWorker = false;
+    if (
+        settings["boidCount"] != Number(form["boidCount"].value) ||
+        settings["maxSpeed"] != Number(form["maxSpeed"].value) ||
+        settings["maxForce"] != Number(form["maxForce"].value) ||
+        settings["neighbohoodSize"] != Number(form["neighbohoodSize"].value) ||
+        settings["colorSeperation"] != form["colorSeperation"].value
+    ) {
+        restartWebWorker = true;
+    }
+    //check if boidCount has changed
+    if (settings["boidCount"] != Number(form["boidCount"].value)) {
+        //need to add or remove boids
 
-		//check if we need to remove or add boids
-		if (settings["boidCount"] > Number(form["boidCount"].value)) {
-			//need to remove boids
+        //check if we need to remove or add boids
+        if (settings["boidCount"] > Number(form["boidCount"].value)) {
+            //need to remove boids
 
-			const boidsToRemove =
-				settings["boidCount"] - Number(form["boidCount"].value);
-			if (boidsToRemove <= 0) {
-				//user put in negative ammount of boids
-				console.error("Cant have negative boids");
-			} else {
-				instancedBoids.dispose();
-				let newSettings = { ...settings };
-				newSettings["boidCount"] = Number(form["boidCount"].value);
-				instancedBoids = new Boids(scene, newSettings);
-				restartWebWorker = true;
-			}
-		} else {
-			//need to add boids
+            const boidsToRemove =
+                settings["boidCount"] - Number(form["boidCount"].value);
+            if (boidsToRemove <= 0) {
+                //user put in negative ammount of boids
+                console.error("Cant have negative boids");
+            } else {
+                instancedBoids.dispose();
+                let newSettings = { ...settings };
+                newSettings["boidCount"] = Number(form["boidCount"].value);
+                instancedBoids = new Boids(scene, newSettings);
+                restartWebWorker = true;
+            }
+        } else {
+            //need to add boids
 
-			//calculate the ammount of boids needed
-			const boidsToAdd = Number(form["boidCount"].value);
-			-settings["boidCount"];
-			if (boidsToAdd <= 0) {
-				//user put in negative number
-				console.error("Can't have negative boids");
-			} else {
-				let newSettings = { ...settings };
-				newSettings["boidCount"] = Number(form["boidCount"].value);
-				instancedBoids.dispose();
-				instancedBoids = new Boids(scene, newSettings);
-				restartWebWorker = true;
-			}
-		}
-	}
-	//check if box size changed
-	if (settings["boxSize"] != Number(form["boxSize"].value)) {
-		//box size changed
+            //calculate the ammount of boids needed
+            const boidsToAdd = Number(form["boidCount"].value);
+            -settings["boidCount"];
+            if (boidsToAdd <= 0) {
+                //user put in negative number
+                console.error("Can't have negative boids");
+            } else {
+                let newSettings = { ...settings };
+                newSettings["boidCount"] = Number(form["boidCount"].value);
+                instancedBoids.dispose();
+                instancedBoids = new Boids(scene, newSettings);
+                restartWebWorker = true;
+            }
+        }
+    }
+    //check if box size changed
+    if (settings["boxSize"] != Number(form["boxSize"].value)) {
+        //box size changed
 
-		//change box size setting first
-		settings["boxSize"] = Number(form["boxSize"].value);
+        //change box size setting first
+        settings["boxSize"] = Number(form["boxSize"].value);
 
-		//than run function that changes the box displayed
-		[boxgeo, boxmat, box] = changeBox(boxgeo, boxmat, box, scene, settings);
-	}
+        //than run function that changes the box displayed
+        [boxgeo, boxmat, box] = changeBox(boxgeo, boxmat, box, scene, settings);
+    }
 
-	if (settings["sphere"] != form["sphere"].checked) {
-		//change shape to sphere
+    if (settings["sphere"] != form["sphere"].checked) {
+        //change shape to sphere
 
-		if (form["sphere"].checked) {
-			scene.remove(box);
-			boxgeo.dispose();
-			boxgeo = new THREE.SphereBufferGeometry(settings.boxSize, 32, 32);
-			box = new THREE.Mesh(boxgeo, boxmat);
-			scene.add(box);
+        if (form["sphere"].checked) {
+            scene.remove(box);
+            boxgeo.dispose();
+            boxgeo = new THREE.SphereBufferGeometry(settings.boxSize, 32, 32);
+            box = new THREE.Mesh(boxgeo, boxmat);
+            scene.add(box);
 
-			document.getElementById("containerLabel")!.innerHTML =
-				"Sphere Size";
-		} else {
-			changeBox(boxgeo, boxmat, box, scene, settings);
+            document.getElementById("containerLabel")!.innerHTML =
+                "Sphere Size";
+        } else {
+            changeBox(boxgeo, boxmat, box, scene, settings);
 
-			document.getElementById("containerLabel")!.innerHTML = "Box Size";
-		}
+            document.getElementById("containerLabel")!.innerHTML = "Box Size";
+        }
 
-		settings["sphere"] = form["sphere"].checked;
-	}
-	//update all other settings
+        settings["sphere"] = form["sphere"].checked;
+    }
+    //update all other settings
 
-	settings["maxSpeed"] = Number(form["maxSpeed"].value);
+    settings["maxSpeed"] = Number(form["maxSpeed"].value);
 
-	settings["maxForce"] = Number(form["maxForce"].value);
+    settings["maxForce"] = Number(form["maxForce"].value);
 
-	settings["neighbohoodSize"] = Number(form["neighbohoodSize"].value);
+    settings["neighbohoodSize"] = Number(form["neighbohoodSize"].value);
 
-	settings["randomHome"] = form["randomHome"].checked;
+    settings["randomHome"] = form["randomHome"].checked;
 
-	settings["colorSeperation"] = form["color"].checked;
+    settings["colorSeperation"] = form["color"].checked;
 
-	settings["boidCount"] = Number(form["boidCount"].value);
-	if (restartWebWorker) {
-		startThreads(settings["boidCount"], maxThreads);
-	}
+    settings["boidCount"] = Number(form["boidCount"].value);
+    if (restartWebWorker) {
+        startThreads(settings["boidCount"], maxThreads);
+    }
 });
 
 const { scene, renderer, camera, boids } = setup(settings);
@@ -156,9 +156,9 @@ const { scene, renderer, camera, boids } = setup(settings);
 let instancedBoids = new Boids(scene, settings);
 
 let boxgeo: any = new THREE.BoxBufferGeometry(
-	settings.boxSize,
-	settings.boxSize,
-	settings.boxSize
+    settings.boxSize,
+    settings.boxSize,
+    settings.boxSize
 );
 let boxmat = new THREE.MeshBasicMaterial({ color: 0x000000 });
 boxmat.transparent = true;
@@ -169,33 +169,33 @@ scene.add(box);
 
 //function to create WebWorkers
 const startThreads = (boidCount: number, maxThreads: number) => {
-	//check if threads exist
-	if (floatThreads) {
-		executionPaused = true;
-		//set them to float64Array of 69s
-		meta[0] = 69;
-		meta[1] = 69;
-		sharedMemory = undefined;
-		floatThreads = undefined;
-		meta = undefined;
-	}
+    //check if threads exist
+    if (floatThreads) {
+        executionPaused = true;
+        //set them to float64Array of 69s
+        meta[0] = 69;
+        meta[1] = 69;
+        sharedMemory = undefined;
+        floatThreads = undefined;
+        meta = undefined;
+    }
 
-	const workerValues = initWorkers(boidCount, settings);
-	sharedMemory = workerValues.buffer;
-	threadCount = workerValues.count;
+    const workerValues = initWorkers(boidCount, settings);
+    sharedMemory = workerValues.buffer;
+    threadCount = workerValues.count;
 
-	floatThreads = new Float64Array(
-		sharedMemory,
-		0,
-		(sharedMemory.byteLength - 8) / 8
-	); //-8 to remove the 8 byte metadata
-	meta = new Int32Array(sharedMemory, sharedMemory.byteLength - 8);
-	for (let i = 0; i < boidCount; i++) {
-		floatThreads[i * 9] = instancedBoids.startingPositions[i].position.x;
-		floatThreads[i * 9 + 1] = instancedBoids.startingPositions[i].position.y;
-		floatThreads[i * 9 + 2] = instancedBoids.startingPositions[i].position.z;
-	}
-	executionPaused = false;
+    floatThreads = new Float64Array(
+        sharedMemory,
+        0,
+        (sharedMemory.byteLength - 8) / 8
+    ); //-8 to remove the 8 byte metadata
+    meta = new Int32Array(sharedMemory, sharedMemory.byteLength - 8);
+    for (let i = 0; i < boidCount; i++) {
+        floatThreads[i * 9] = instancedBoids.startingPositions[i].position.x;
+        floatThreads[i * 9 + 1] = instancedBoids.startingPositions[i].position.y;
+        floatThreads[i * 9 + 2] = instancedBoids.startingPositions[i].position.z;
+    }
+    executionPaused = false;
 };
 let executionPaused = false;
 //animation loop
@@ -210,19 +210,19 @@ let newTick = 0;
 // }
 scene.add(instancedMesh);
 let dummy = new THREE.Object3D();
-renderer.setAnimationLoop(function () {
-	if (!executionPaused) {
-		if (newTick > 1000) {
-			newTick = 0;
-		}
-		newTick++;
-		// track fps with stats module
-		stats.begin();
-		// notify all threads to start frame using Atomics
-		Atomics.store(meta, 0, newTick);
-		Atomics.notify(meta, 0);
-		instancedBoids.updateBoids(floatThreads);
-		renderer.render(scene, camera);
-		stats.end();
-	}
+renderer.setAnimationLoop(function() {
+    if (!executionPaused) {
+        if (newTick > 1000) {
+            newTick = 0;
+        }
+        newTick++;
+        // track fps with stats module
+        stats.begin();
+        // notify all threads to start frame using Atomics
+        Atomics.store(meta, 0, newTick);
+        Atomics.notify(meta, 0);
+        instancedBoids.updateBoids(floatThreads);
+        renderer.render(scene, camera);
+        stats.end();
+    }
 });
